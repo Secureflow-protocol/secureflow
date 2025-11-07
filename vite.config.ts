127b37ca6101d77f804bb618c72595df5bd136b4
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-11-07 19:16:36 +0100

    Enhance Vite configuration: Add support for ES module path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig(() => {
